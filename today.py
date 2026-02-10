e0c13e37107a6060e96d15322c4f589cc56d9770
Buse <buseozgur@Buse-MacBook-Air.local>
2026-02-10 14:59:33 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,7 +1,9 @@
-'''
-This is the module docstring
-'''
+
+"""A module computing buddy pair names for the day"""
+
 def my_name_is():
+    """Returns the GitHub username"""
     return "buseozgur"
 def my_buddy_is():
-    return "buseozgur"
+    """Returns the buddy's username"""
+    return "buseozgur" #define my buddy name
